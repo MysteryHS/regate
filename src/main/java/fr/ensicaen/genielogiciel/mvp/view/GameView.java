d0f177d64cd9a505bc9dcd20112a17834fa805a8
Romain CAILLY <romain.cailly@ecole.ensicaen.fr>
2022-10-04 12:31:49 +0200

    modification du fichier fxml : ajout de la carte et du champ chrono. Debut du code pour afficher l'eau/le sable sur la carte

--- src/main/java/fr/ensicaen/genielogiciel/mvp/view/GameView.java
+++ src/main/java/fr/ensicaen/genielogiciel/mvp/view/GameView.java
@@ -6,11 +6,14 @@ import fr.ensicaen.genielogiciel.mvp.presenter.IGameView;
 import fr.ensicaen.genielogiciel.mvp.presenter.UserAction;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Ellipse;
 import javafx.stage.Stage;
@@ -21,6 +24,7 @@ public class GameView implements IGameView {
     private static Stage _stage;
     private GamePresenter _gamePresenter;
     private Ellipse _boat;
+
     @FXML
     private AnchorPane _base;
 
@@ -67,14 +71,58 @@ public class GameView implements IGameView {
         }
     }
 
+    /* stage / scene
+            superposer
+                gridview
+                    map
+                anchorpane
+                    boat
+
+
+     */
     public static class GameViewFactory {
+
+        private static int mapHeight = 500;
+        private static int mapWidth = 500;
+
+        @FXML
+        private AnchorPane map;
+
+
+
         private GameViewFactory() {
             // Factory class as Utility class where the constructor is private
         }
 
+
+
+
+        private static void fillMap(AnchorPane pane) {
+
+            int nbCol = 20;
+            int nbRow = 20;
+            TileUI tile;
+
+            for(int col=0 ; col<nbCol ; col++) {
+                for(int row=0 ; row<nbRow ; row++) {
+                    tile = new TileUI();
+                    tile.setFitWidth((mapWidth/nbCol));
+                    tile.setFitHeight((mapHeight/nbRow));
+                    tile.setLayoutX((mapWidth/nbCol)*col);
+                    tile.setLayoutY((mapHeight/nbRow)*row);
+                    pane.getChildren().add(tile);
+                }
+            }
+        }
+
+
         public static GameView createView() throws IOException {
             FXMLLoader loader = new FXMLLoader(LoginView.class.getResource("SpotMap.fxml"), Main.getMessageBundle());
             Parent root = loader.load();
+            root.resize(mapWidth,mapHeight);
+
+            fillMap((AnchorPane) root.lookup("#map"));
+
             GameView view = loader.getController();
             Scene scene = new Scene(root, 800, 600);
             Stage stage = new Stage();
@@ -85,6 +133,8 @@ public class GameView implements IGameView {
                 KeyCode code = event.getCode();
                 view.handleKeyPressed(code);
             });
+
+
             return view;
         }
     }
