93109e7b048a0ec1ee0fd4fae1183a54512d511b
Flicourt Emeline <emeline.flicourt@ecole.ensicaen.fr>
2022-10-04 12:31:05 +0200

    Map Fin séance 1

--- src/main/java/fr/ensicaen/genielogiciel/mvp/model/map/Map.java
+++ src/main/java/fr/ensicaen/genielogiciel/mvp/model/map/Map.java
@@ -1,30 +1,50 @@
 package fr.ensicaen.genielogiciel.mvp.model.map;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Map {
-    private final int _width;
-    private final int _height;
-    private final int _nbBuoy;
+    private int _width;
+    private int _height;
+    private int _nbBuoy;
     private List<Buoy> _buoys = new ArrayList<Buoy>();
 
-    public Map(int width, int height, int nbBuoy) {
-        _width = width;
-        _height = height;
-        _nbBuoy = nbBuoy;
+    public static void main(String[] args) throws IOException {
+        Map m = new Map("./src/main/resources/fr/ensicaen/genielogiciel/mvp/maps/carte1.txt");
+        m.displayInformationMap();
     }
 
-    public String readFirstLineInFile(String filename) throws IOException{
-        try {
-            File inputFile = new File(filename);
-            Scanner myReader = new Scanner(inputFile);
-            String line = myReader.nextLine();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+    public Map(String filename) throws IOException {
+        readFirstLineInFile(filename);
+    }
+
+    private void readFirstLineInFile(String filename) throws IOException {
+        int XCoordinate;
+        int YCoordinate;
+        File inputFile = new File(filename);
+        Scanner myReader = new Scanner(inputFile);
+        String firstLine = myReader.nextLine();
+        String[] delims = firstLine.split(" ");
+        _width = Integer.parseInt(delims[0]);
+        _height = Integer.parseInt(delims[1]);
+        _nbBuoy = Integer.parseInt(delims[2]);
+        for (int i = 0 ; i < _nbBuoy ; i++) {
+            String dataBuoy = myReader.nextLine();
+            delims = dataBuoy.split(" ");
+            XCoordinate = Integer.parseInt(delims[0]);
+            YCoordinate = Integer.parseInt(delims[1]);
+            Buoy b = new Buoy(XCoordinate, YCoordinate);
+            _buoys.add(b);
         }
-        return line;
+    }
+
+    private void displayInformationMap(){
+        System.out.println("Width = " + _width);
+        System.out.println("Height = " + _height);
+        System.out.println("nbBuoy = " + _nbBuoy);
     }
 }
