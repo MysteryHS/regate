63fca3b35da871e35a7a6b363dadf8e2b9415ebd
Steimetz <tangui.steimetz@ecole.ensicaen.fr>
2022-10-04 11:33:37 +0200

    Wind package - first enum

---
+++ src/main/java/fr/ensicaen/genielogiciel/mvp/model/map/wind/windDirection.java
@@ -0,0 +1,12 @@
+package fr.ensicaen.genielogiciel.mvp.model.map.wind;
+
+public enum windDirection {
+    NORTH,
+    NORTH_EAST,
+    EAST,
+    SOUTH_EAST,
+    SOUTH,
+    SOUTH_WEST,
+    WEST,
+
+}
