b4f70ccd87c4c97a991c9afbc05020d52efdb914
Steimetz <tangui.steimetz@ecole.ensicaen.fr>
2022-10-04 21:00:41 +0200

    Wind Proxy DONE - wait for merge into MASTER

---
+++ src/main/java/fr/ensicaen/genielogiciel/mvp/model/map/wind/Wind.java
@@ -0,0 +1,5 @@
+package fr.ensicaen.genielogiciel.mvp.model.map.wind;
+public interface Wind {
+    public int getWindKnot();
+    public WindDirection getWindDirection();
+}
---
+++ src/main/java/fr/ensicaen/genielogiciel/mvp/model/map/wind/WindServer.java
@@ -0,0 +1,27 @@
+package fr.ensicaen.genielogiciel.mvp.model.map.wind;
+
+public class WindServer implements Wind {
+    private final double _longitude;
+    private final double _latitude;
+    private WindData _windData;
+
+    public WindServer(double longitude, double latitude) {
+        _longitude = longitude;
+        _latitude = latitude;
+        _windData = queryWindDataFromJSON();
+    }
+
+    private WindData queryWindDataFromJSON() {
+        return new WindData(WindDirection.SOUTH, 3);
+    }
+
+    @Override
+    public int getWindKnot() {
+        return _windData.getKnot();
+    }
+
+    @Override
+    public WindDirection getWindDirection() {
+        return _windData.getDirection();
+    }
+}
