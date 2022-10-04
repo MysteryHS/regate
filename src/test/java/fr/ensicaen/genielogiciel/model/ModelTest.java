b29786811bb4a496bbd599e4e94851e3ab4c180f
Benjamin MAIGNAN <benjamin.maignan@ecole.ensicaen.fr>
2022-10-04 12:12:30 +0200

    Corrected error in ModelTest

--- src/test/java/fr/ensicaen/genielogiciel/model/ModelTest.java
+++ src/test/java/fr/ensicaen/genielogiciel/model/ModelTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.wjunit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ModelTest {
 
