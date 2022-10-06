014f42e42172e975d486d283bc178ec45c9d6fea
TanguiS <tangui.steimetz@ecole.ensicaen.fr>
2022-10-06 18:52:46 +0200

    coverage 100% - might need more testing.

--- src/test/java/fr/ensicaen/genielogiciel/mvp/model/ship/ShipModelTest.java
+++ src/test/java/fr/ensicaen/genielogiciel/mvp/model/ship/ShipModelTest.java
@@ -1,25 +1,25 @@
 package fr.ensicaen.genielogiciel.mvp.model.ship;
 
 import fr.ensicaen.genielogiciel.mvp.model.map.wind.Wind;
-import fr.ensicaen.genielogiciel.mvp.model.map.wind.WindData;
 import fr.ensicaen.genielogiciel.mvp.model.map.wind.WindDirection;
 import fr.ensicaen.genielogiciel.mvp.model.map.wind.WindProxy;
 import fr.ensicaen.genielogiciel.mvp.model.ship.crew.NormalCrew;
 import fr.ensicaen.genielogiciel.mvp.model.ship.sail.NormalSail;
-import fr.ensicaen.genielogiciel.mvp.presenter.GamePresenter;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
 class ShipModelTest {
     private ShipModel _ship;
     private double _angle;
     private double _knot;
-    private double _acc;
+    private double _speedRatio;
+    private double _maxSpeed;
     @Mock
     private Wind _mockedWind;
     @Mock
@@ -28,12 +28,11 @@ class ShipModelTest {
     @BeforeEach
     void setUp() {
         _mockedWind = mock(WindProxy.class);
+        _knot = 6.;
         _mockedPolar = mock(DataPolar.class);
         doReturn(WindDirection.SOUTH).when(_mockedWind).getWindDirection();
-        doReturn(4.).when(_mockedWind).getWindKnot();
-        _knot = _mockedWind.getWindKnot();
+        doReturn(_knot).when(_mockedWind).getWindKnot();
         _angle = 60.;
-        _acc = 0.02;
     }
     @AfterEach
     void tearDown() {
@@ -41,23 +40,15 @@ class ShipModelTest {
     }
 
     @Test
-    void todo() {
+    void shipSpeedShouldBeEqualsToMaxSpeed() {
         _ship = new ShipModel(new NormalSail(), new NormalCrew(), _mockedWind, _mockedPolar );
-        System.out.println(_ship.getX());
-        System.out.println(_ship.getDx());
-        System.out.println(_ship.getAngle());
+        _speedRatio = _ship.getSpeedRatio();
+        _maxSpeed = new TestDataPolar().getPolarValues(_angle, _knot) * _speedRatio;
         doReturn(new TestDataPolar().getPolarValues(_angle, _knot)).when(_mockedPolar).getPolarValues(_angle, _knot);
-        double dx = 0.;
         _ship.rotate(_angle);
         for ( int i = 0; i < 100; i++ ) {
             _ship.move();
-            dx += _acc;
         }
-        System.out.println( (double) Math.round(_ship.getX() * 100.) / 100. );
-        System.out.println(_ship.getDx());
-        System.out.println(dx);
-        System.out.println(_ship.getAngle());
-        assertEquals(_angle, _ship.getAngle());
+        assertEquals(_maxSpeed, Math.sqrt(_ship.getDx()* _ship.getDx() + _ship.getDy()* _ship.getDy()));
     }
-
 }
--- src/test/java/fr/ensicaen/genielogiciel/mvp/model/ship/TestDataPolar.java
+++ src/test/java/fr/ensicaen/genielogiciel/mvp/model/ship/TestDataPolar.java
@@ -6,10 +6,9 @@ public class TestDataPolar {
 
     public double getPolarValues(double angle, double knot) {
         if ( angle < 30. ) {
-            System.out.println("BUG");
+            System.out.println(angle);
             return 0.;
         }
-        System.out.println("angle = " + angle + " knot = " + knot);
         return (0.0019886363636 * angle * knot) + 2.261336363636;
     }
 }
