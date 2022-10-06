581997af7b04ef39cbdb856411ecd7fc67fa9a86
Ducastel Mateo <ducastel.mateo@laposte.net>
2022-10-06 11:07:16 +0200

    Modified wind direction because the direction was inverted

--- src/main/java/fr/ensicaen/genielogiciel/mvp/model/map/wind/WindDirection.java
+++ src/main/java/fr/ensicaen/genielogiciel/mvp/model/map/wind/WindDirection.java
@@ -1,14 +1,14 @@
 package fr.ensicaen.genielogiciel.mvp.model.map.wind;
 
 public enum WindDirection {
-    SOUTH           (180),
-    SOUTH_EAST      (225),
-    EAST            (270),
-    NORTH_EAST      (315),
-    NORTH           (0),
-    NORTH_WEST      (45),
-    WEST            (90),
-    SOUTH_WEST      (135);
+    SOUTH           (0),
+    SOUTH_EAST      (45),
+    EAST            (90),
+    NORTH_EAST      (135),
+    NORTH           (180),
+    NORTH_WEST      (225),
+    WEST            (270),
+    SOUTH_WEST      (315);
 
     private final double _angle;
     WindDirection(double angle) {
