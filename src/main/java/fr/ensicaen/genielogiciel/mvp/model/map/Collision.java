e5e5fe84cec813de7602b190fc516ca271681f3b
Thomas Seng <thomas.seng@ecole.ensicaen.fr>
2022-10-25 10:22:37 +0200

    Creation Collision class

---
+++ src/main/java/fr/ensicaen/genielogiciel/mvp/model/map/Collision.java
@@ -0,0 +1,74 @@
+package fr.ensicaen.genielogiciel.mvp.model.map;
+
+import fr.ensicaen.genielogiciel.mvp.model.ship.ShipModel;
+
+public class Collision {
+    private boolean _collision;
+    private Map _map;
+    private ShipModel _ship;
+
+    public Collision(Map map, ShipModel ship) {
+        _collision = false;
+        _map = map;
+        _ship = ship;
+    }
+
+    private void resetCollision() {
+        _collision = false;
+    }
+
+    public boolean outOfRange() {
+        boolean infX = _ship.getX() + _ship.getDx() < 0;
+        boolean supX = _ship.getX() + _ship.getDx() > _map.getWidth();
+        boolean infY = _ship.getY() + _ship.getDy() < 0;
+        boolean supY = _ship.getY() + _ship.getDy() > _map.getHeight();
+        if (infX || supX || infY || supY) {
+            _collision = true;
+        } else {
+            resetCollision();
+        }
+        return _collision;
+    }
+
+    public boolean collisionWithBuoy() {
+        boolean checkX;
+        boolean checkY;
+
+        for (int i = 0; i < _map.getNbBuoy(); i++) {
+            checkX = _map.getBuoys().get(i).getY() == _ship.getY() + _ship.getDy();
+            checkY = _map.getBuoys().get(i).getY() == _ship.getY() + _ship.getDy();
+            if (checkX && checkY) {
+                _collision = true;
+            } else {
+                resetCollision();
+            }
+        }
+        return _collision;
+    }
+
+    public boolean collisionWithSand() {
+        double nextX = _ship.getX() + _ship.getDx();
+        double nextY = _ship.getY() + _ship.getDy();
+
+        if (_map.getType( (int)nextX, (int)nextY) == '.') {
+            _collision = true;
+        } else {
+            resetCollision();
+        }
+        return _collision;
+    }
+
+    public boolean collisionWithShip(ShipModel another) {
+        double nextX = _ship.getX() + _ship.getDx();
+        double nextY = _ship.getY() + _ship.getDy();
+
+        boolean collision = (another.getX() == nextX) && (another.getY() == nextY);
+
+        if (collision) {
+            _collision = true;
+        } else {
+            resetCollision();
+        }
+        return _collision;
+    }
+}
