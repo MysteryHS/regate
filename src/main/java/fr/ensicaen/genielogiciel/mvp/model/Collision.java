64d753766094d5f6ea9eca3ca4931f0bd18f1ba1
Ducastel Mateo <ducastel.mateo@laposte.net>
2022-10-27 11:25:17 +0200

    Cleaned code of collision

--- src/main/java/fr/ensicaen/genielogiciel/mvp/model/Collision.java
+++ src/main/java/fr/ensicaen/genielogiciel/mvp/model/Collision.java
@@ -4,31 +4,21 @@ import fr.ensicaen.genielogiciel.mvp.model.map.GameMap;
 import fr.ensicaen.genielogiciel.mvp.model.ship.ShipModel;
 
 public class Collision {
-    private boolean _collision;
     final private GameMap _map;
     final private ShipModel _ship;
 
     public Collision(GameMap map, ShipModel ship) {
-        _collision = false;
         _map = map;
         _ship = ship;
     }
 
-    private void resetCollision() {
-        _collision = false;
-    }
 
     public boolean outOfRange() {
         boolean infX = _ship.getX() + _ship.getDx() < 0;
         boolean supX = _ship.getX() + _ship.getDx() + _ship.getWidth() >= _map.getWidth();
         boolean infY = _ship.getY() + _ship.getDy() < 0;
         boolean supY = _ship.getY() + _ship.getDy() + _ship.getHeight() >= _map.getHeight();
-        if (infX || supX || infY || supY) {
-            _collision = true;
-        } else {
-            resetCollision();
-        }
-        return _collision;
+        return infX || supX || infY || supY;
     }
 
     public boolean collisionWithBuoy() {
@@ -38,24 +28,16 @@ public class Collision {
             checkX = _map.getBuoys().get(i).getY() == _ship.getY() + _ship.getDy();
             checkY = _map.getBuoys().get(i).getY() == _ship.getY() + _ship.getDy();
             if (checkX && checkY) {
-                _collision = true;
-            } else {
-                resetCollision();
+                return true;
             }
         }
-        return _collision;
+        return false;
     }
 
     public boolean collisionWithSand() {
         double nextX = _ship.getX() + _ship.getDx();
         double nextY = _ship.getY() + _ship.getDy();
-
-        if (_map.getType( (int)nextX, (int)nextY) == '.') {
-            _collision = true;
-        } else {
-            resetCollision();
-        }
-        return _collision;
+        return _map.getType( (int)nextX, (int)nextY) == '.';
     }
 
     public void collisionWithOtherPlayers(ShipModel another) {
@@ -63,7 +45,6 @@ public class Collision {
     }
 
     private boolean collisionWithSomething() {
-
         return collisionWithSand() || outOfRange(); //collisionWithBuoy() ||
     }
 
